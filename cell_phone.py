6f6a3998fa7a9e9bdc57f1e6704310f51b549628
Akeem <akeem.mighty@gmail.com>
2022-03-18 16:15:40 -0400

    add initial variables to cell phone class

--- cell_phone.py
+++ cell_phone.py
@@ -1,3 +1,13 @@
+from pyexpat import model
+
+
 class CellPhone:
-    def __init__(self):
-        pass
+    def __init__(self, model=""):
+        self.model = model
+        self.phone_number = phone_number
+        self.contacts = dict()
+        self.messages = []
+        is_vibrate = False
+
+
+    
