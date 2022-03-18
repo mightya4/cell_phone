d245661738a0d386977ccbc6c2fc11c76de31453
Akeem <akeem.mighty@gmail.com>
2022-03-18 16:27:22 -0400

    added main file; added receive text message functionality

--- cell_phone.py
+++ cell_phone.py
@@ -4,10 +4,11 @@ from pyexpat import model
 class CellPhone:
     def __init__(self, model=""):
         self.model = model
-        self.phone_number = phone_number
+        self.phone_number = 0
         self.contacts = dict()
         self.messages = []
         is_vibrate = False
 
-
-    
+    def receive_txt_message(self, message):
+        print(message)
+        self.messages.append(message)
---
+++ main.py
@@ -0,0 +1,5 @@
+from cell_phone import CellPhone
+
+new_cellphone = CellPhone("Galaxy S22")
+print(new_cellphone.model)
+
